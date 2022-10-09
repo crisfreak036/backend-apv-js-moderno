555cfc234d53a0af9894be4f1fb2ee053648e301
crisfreak <cristopher.sotog@utem.cl>
2022-10-09 16:32:23 -0300

    [add] validación de preexistencia del usuario

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -2,16 +2,29 @@ import Veterinario from '../models/Veterinario.js'
 
 const registrar = async (req, res, next) => {
     const { body } = req;
+
+    // Comprueba que el body tenga información (se puede utilizar JOI para validar elementos de la req)
     if (Object.keys(body).length === 0) {
-        return res.status(422).json({ code: 422, error: true, data: undefined });
+        return res.status(400).json({ code: 400, error: true, data: undefined });
     }
+
+    const { email, password, nombre } = body;
+
+    // Comprueba que el usuario exista previamente
+    const usuario = await Veterinario.findOne({email: email});
+    if (usuario) {
+        return res.status(400).json({ code: 400, error: true, message: "user already exists", data: undefined });
+    }
+
+
     try {
+        // Crea al usuario
         const veterinario = new Veterinario(body);
         const veterinarioGuardado = await veterinario.save();
-        res.status(201).json({ code: 201, error: false, data: veterinarioGuardado });
+        res.status(201).json({ code: 201, error: false, message: "ok",data: veterinarioGuardado });
     } catch (error) {
        console.log(error);
-       res.status(422).json({ code: 422, error: true, data: undefined });
+       res.status(422).json({ code: 422, error: true, message: "Something wrong",data: undefined });
     }
 }
 
