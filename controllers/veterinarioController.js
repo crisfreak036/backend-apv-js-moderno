b163062fcc8302d018e5e25625eb2d2ea33b4ae3
crisfreak <cristopher.sotog@utem.cl>
2022-10-09 14:45:49 -0300

    [add] almacenado del vetrinario la función registrar de veterinarioController

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,6 +1,18 @@
+import Veterinario from '../models/Veterinario.js'
 
-const registrar = (req, res, next) => {
-    res.json({message:'Registrando Usuario...'});
+const registrar = async (req, res, next) => {
+    const { body } = req;
+    if (Object.keys(body).length === 0) {
+        return res.status(422).json({ code: 422, error: true, data: undefined });
+    }
+    try {
+        const veterinario = new Veterinario(body);
+        const veterinarioGuardado = await veterinario.save();
+        res.status(201).json({ code: 201, error: false, data: veterinarioGuardado });
+    } catch (error) {
+       console.log(error);
+       res.status(422).json({ code: 422, error: true, data: undefined });
+    }
 }
 
 const perfil = (req, res, next) => {
