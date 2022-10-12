865fba9b5f5ea360dd36bcb1be779d554ac2d5da
crisfreak <cristopher.sotog@utem.cl>
2022-10-11 23:10:11 -0300

    [add] protección de ruta mediante autenticación de JWT en la ruta /perfil

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { registrar, perfil, confirmar, autenticar } from '../controllers/veterinarioController.js'
+import authHandler from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
-router.get("/perfil", perfil);
-router.get("/confirmar/:token", confirmar);
 
+// Public Router
 router.post("/", registrar);
+router.get("/confirmar/:token", confirmar);
 router.post("/login", autenticar);
+
+// Private Router
+router.get("/perfil", authHandler, perfil);
 
 export default router;
