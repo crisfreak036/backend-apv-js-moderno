60f3c9c8eaed187f96431dddc496c5c871b6bffd
crisfreak <cristopher.sotog@utem.cl>
2022-10-12 23:06:26 -0300

    [update] rutas /olvide-password/:token uniendolas en una solo línea

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -9,8 +9,7 @@ router.post("/", registrar);
 router.get("/confirmar/:token", confirmar);
 router.post("/login", autenticar);
 router.post("/olvide-password", olvidePassword);
-router.get("/olvide-password/:token", comprobarToken);
-router.post("/olvide-password/:token", nuevoPassword);
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
 
 // Private Router
 router.get("/perfil", authHandler, perfil);
