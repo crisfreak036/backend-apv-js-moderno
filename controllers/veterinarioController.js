5245009aeb3e41d5024f72ac650231071c48b0b2
crisfreak <cristopher.sotog@utem.cl>
2022-10-12 23:07:36 -0300

    [add] controlador olvidePassword para la ruta POST /veterinarios/olvide-password

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,5 +1,6 @@
 import Veterinario from '../models/Veterinario.js';
 import generarJWT from '../helpers/generarJWT.js';
+import generarId from "../helpers/generarId.js";
 
 const registrar = async (req, res, next) => {
     const { body } = req;
@@ -88,9 +89,49 @@ const autenticar = async (req, res, next) => {
     }
 }
 
+const olvidePassword = async (req, res, next) => {
+    const { body } = req;
+
+    // Comprueba que el body tenga información (se puede utilizar JOI para validar elementos de la req)
+    if (Object.keys(body).length === 0) {
+        return res.status(400).json({ code: 400, error: true, data: undefined });
+    }
+
+    const { email } = body;
+
+    try {
+        const usuario = await Veterinario.findOne({email: email});
+        if (!usuario) {
+            return res.status(404).json({ code: 404, error: true, message: "Not Found", data: undefined });
+        }
+
+        if (!usuario.confirmado) {
+            return res.status(400).json({ code: 400, error: true, message: "Confirmation is required", data: undefined });
+        }
+        usuario.token = generarId();
+        usuario.save();
+        res.status(200).json({code: 200, error: false, message: 'Correo de recuperación enviado', data: undefined});
+    } catch (error) {
+        console.log(error);
+        res.status(422).json({ code: 422, error: true, message: "Something wrong",data: undefined });
+    }
+    
+}
+
+const comprobarToken = (req, res, next) => {
+    res.json({message:'Ya mande mi token'});
+}
+
+const nuevoPassword = (req, res, next) => {
+    res.json({message:'Toca resetear edl password'});
+}
+
 export {
     registrar,
     perfil,
     confirmar,
-    autenticar
+    autenticar,
+    olvidePassword,
+    comprobarToken,
+    nuevoPassword
 }
