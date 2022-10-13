63c530e60c038c14abfd89297ac736e308f4a3ce
crisfreak <cristopher.sotog@utem.cl>
2022-10-12 23:03:48 -0300

    [add] rutas finales del veterianrio

--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registrar, perfil, confirmar, autenticar } from '../controllers/veterinarioController.js'
+import { registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword } from '../controllers/veterinarioController.js'
 import authHandler from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -8,6 +8,9 @@ const router = express.Router();
 router.post("/", registrar);
 router.get("/confirmar/:token", confirmar);
 router.post("/login", autenticar);
+router.post("/olvide-password", olvidePassword);
+router.get("/olvide-password/:token", comprobarToken);
+router.post("/olvide-password/:token", nuevoPassword);
 
 // Private Router
 router.get("/perfil", authHandler, perfil);
