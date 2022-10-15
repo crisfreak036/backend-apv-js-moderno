4774f917f2a64612822bbc8e175c1002ba1dd04b
crisfreak <cristopher.sotog@utem.cl>
2022-10-15 20:51:03 -0300

    [add] controladores obtenerPaciente, actualizarPaciente, eliminarPaciente. obtenerPaciente está finalizado

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -39,7 +39,66 @@ const agregarPaciente = async (req, res, next) => {
     }
 }
 
+const obtenerPaciente = async (req, res, next) => {
+    const { id } = req.params;
+    const { user } = req;
+    try {
+        const paciente = await Paciente.findById(id);
+        console.log(paciente);
+        if (!paciente)  return res.status(404).json({ code: 404, error: true, message: "Paciente no encontrado", data: {} });
+        if (paciente.veterinario._id.toString() !== user._id.toString()) return res.status(403).json({ code: 403, error: true, message: "Acción no valida", data: {} });
+        res.status(200).json({ code: 200, error: false, message: "ok", data: paciente });
+    } catch (error) {
+        console.log(error);
+        res.status(422).json({ code: 422, error: true, message: "Something wrong",data: undefined });
+    }
+}
+
+const actualizarPaciente = async (req, res, next) => {
+    const { id } = req.params;
+    const { body } = req;
+
+    if (Object.keys(body).length === 0) {
+        return res.status(400).json({ code: 400, error: true, data: undefined });
+    }
+
+    try {
+        let paciente = await Paciente.findById(id);
+        if (!paciente)  return res.status(404).json({ code: 404, error: true, message: "Paciente no encontrado", data: {} });
+
+        const newData = {...paciente, ...body};
+        paciente = {...newData};
+        // await paciente.save();
+
+        res.status(200).json({ code: 200, error: false, message: "ok", data: newData });
+    } catch (error) {
+        console.log(error);
+        res.status(422).json({ code: 422, error: true, message: "Something wrong",data: undefined });
+    }
+}
+
+const eliminarPaciente = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const paciente = await Paciente.findById(id);
+
+        if (!paciente)  return res.status(404).json({ code: 404, error: true, message: "Paciente no encontrado", data: {} });
+
+        // await paciente.delete();
+
+        res.status(200).json({ code: 200, error: false, message: "ok", data: paciente });
+    } catch (error) {
+        console.log(error);
+        res.status(422).json({ code: 422, error: true, message: "Something wrong",data: undefined });
+    }
+}
+
+
 export {
     obtenerPacientes,
-    agregarPaciente
+    obtenerPaciente,
+    agregarPaciente,
+    actualizarPaciente, 
+    eliminarPaciente
 }
